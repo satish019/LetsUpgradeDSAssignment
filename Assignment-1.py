f9404ed8beb4edf89c754b35a6ba4b8c1e989651
Satish <bokkasatish019@gmail.com>
2020-11-09 21:47:35 +0530

    Add files via upload

---
+++ Assignment-1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+# Solution 1 : RAINBOW
+
+
+# In[2]:
+
+
+#2
+print ("LETS UPGRADE")
+
+
+# In[3]:
+
+
+#3
+cp= input()
+sp= input()
+if (sp>cp):
+  print ("Profit")
+elif (cp>sp):
+  print("Loss")
+else:
+  print ("Neither")
+
+
+# In[4]:
+
+
+#4
+eu=float (input())
+inr=eu*80
+print(inr)
+
+
+# In[ ]:
+
+
+
+
